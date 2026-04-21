7d633518ded323469469fa15722dd8e84d4be02c
Muthu-laks <vmuthulakshmi1803@gmail.com>
2026-04-21 15:24:16 +0530

    Updated the loki

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 from fastapi import FastAPI, HTTPException, Request
 import logging
 import json
+from logging_loki import LokiHandler
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "time": self.formatTime(record),
+        }
+        return json.dumps(log_record)
+
+logger = logging.getLogger("app")
+
+handler = logging.StreamHandler()
+handler.setFormatter(JsonFormatter())
+
+# Add Loki handler
+loki_handler = LokiHandler(
+    url="http://loki-gateway.loki.svc.cluster.local/loki/api/v1/push",  # Update with your Loki URL if needed
+    tags={"application": "fast_api"},
+    version="1",
+)
+loki_handler.setFormatter(JsonFormatter())
+logger.addHandler(loki_handler)
+
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+app = FastAPI()
 
 
 class JsonFormatter(logging.Formatter):
@@ -36,7 +65,7 @@ async def log_requests(request: Request, call_next):
 
 @app.get("/")
 def read_root():
-    logger.info("Root endpoint called")
+    logger.info("Root endpoint called ")
     return {"Hello": "Muthu"}
 
 
