ee1878a1538bfcaaab03d5b1950d4befdad40489
Muthu-laks <vmuthulakshmi1803@gmail.com>
2026-04-21 12:53:37 +0530

    updated the route

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from logger import fastapi_logger
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "Muthu"}
+
+
+@app.get("/welcome")
+def welcome_root():
+    return {"Welcome": "Sharvesh"}
+
+
+@app.get("/error")
+def simulate_error():
+    fast_api_logger.error("Simulated error triggered")
+    raise HTTPException(status_code=400, detail="This is a simulated error")
+
