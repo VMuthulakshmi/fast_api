6de92fa35e86eacdfdd234bc6e6665a42b42cc68
Muthu-laks <vmuthulakshmi1803@gmail.com>
2026-04-21 13:05:37 +0530

    Added the logger

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from fastapi import FastAPI, HTTPException, Request
-from logger import fastapi_logger
+import logging
+
+# Create logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
+    logger.info("Root endpoint called")
     return {"Hello": "Muthu"}
 
 
 @app.get("/welcome")
 def welcome_root():
-    return {"Welcome": "Sharvesh"}
+    logger.info("Welcome endpoint called")
+    return {"Welcome": "to FastAPI with Loki logging!"}
 
 
 @app.get("/error")
 def simulate_error():
-    fast_api_logger.error("Simulated error triggered")
+    logger.error("Error endpoint triggered")
     raise HTTPException(status_code=400, detail="This is a simulated error")
-
